47d24f64aabd1fbe019cc81f6e5e38781603bd71
Ayomide Akintan <emzyakints2005@gmail.com>
2025-01-19 22:57:09 +0100

    added nigeria flag with single loop

---
+++ dec2024xmasAssignment/NigeriaFlagSingleLoop.java
@@ -0,0 +1,13 @@
+package dec2024xmasAssignment;
+
+public class NigeriaFlagSingleLoop {
+    public static void main(String[] args) {
+        String pattern = "* * * * = = = = = = =\n"
+                + "* * * * = = = = = = =\n"
+                + "* * * * = = = = = = =\n"
+                + "= = = = = = = = = = =\n"
+                + "= = = = = = = = = = =\n"
+                + "= = = = = = = = = = =";
+        System.out.println(pattern);
+    }
+}
