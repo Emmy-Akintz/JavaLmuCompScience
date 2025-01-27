61804e40d45f39cd728fa01844017794eb785f51
Ayomide Akintan <emzyakints2005@gmail.com>
2025-01-28 00:10:50 +0100

    Added code for prime number

---
+++ Prime.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[4][3];
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("Enter for index [" + i + ", " + j + "]:");
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+
+        // print the prime numbers
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (isPrimeNumber(arr[i][j])) {
+                    System.out.print(arr[i][j] + ", ");
+                }
+            }
+        }
+    }
+
+    public static boolean isPrimeNumber(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
